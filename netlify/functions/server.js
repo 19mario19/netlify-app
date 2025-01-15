00a144d8d0d2622b1e03fa9fc2167ea7e55ba6dc
Mario M <107927311+19mario19@users.noreply.github.com>
2025-01-15 18:07:08 +0000

    changes

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -5,11 +5,11 @@ const app = express()
 
 // Basic routes that redirect to static files
 app.get("/", (req, res) => {
-  res.redirect("/home/index.html")
+  res.redirect("/home/home.html")
 })
 
 app.get("/about", (req, res) => {
-  res.redirect("/about/index.html")
+  res.redirect("/about/about.html")
 })
 
 exports.handler = serverless(app)
